235f4f0229cdc76903d2c166108489265b98d602
pradeepambati <pradeepreddy.ambati@gmail.com>
2017-08-22 11:43:34 -0400

    added a example

---
+++ examples/test.cpp
@@ -0,0 +1,22 @@
+#include "approx_counter.h"
+#include <iostream>
+#include <pthread.h>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    // Declare the counter as an integer counter
+    approx_counter<int> ctr;
+    
+    int incr_count = 64;
+    for (int i =0; i < incr_count; i++)
+    {
+        ++ctr; // Increment the counter.
+    }
+
+    // Print the counter value. 
+    // Use the () operator for accessing the value.
+    cout << "Counter value is " << ctr() << endl;
+    return 0;
+}
